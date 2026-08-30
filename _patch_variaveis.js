ece3a5aac1844872d9ed903e4178d2c654725714
Matheus <matheus@webooter.com>
2026-08-29 20:24:38 -0400

    chore(i18n): remove temp files patch variaveis

--- _patch_variaveis.js
+++
@@ -1,7 +0,0 @@
-import fs from "node:fs";
-const P = "src/components/app/templates/TemplatesClient.tsx";
-let c = fs.readFileSync(P, "utf8");
-c = c.replace(/Use variÃ¡veis:/g, "Use variáveis:");
-c = c.replace(/com variaveis, PIX/g, "com variáveis, PIX");
-fs.writeFileSync(P, c, "utf8");
-console.log("ok");
